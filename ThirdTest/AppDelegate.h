69f2fabe73f7a8ff99118a0f8e42de7551d67e40
wangbo <wangbo@pset.suntec.net>
2015-04-20 16:37:26 +0800

    Initial Commit

---
+++ ThirdTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdTest
+//
+//  Created by iOSDev on 15/4/20.
+//  Copyright (c) 2015年 iOSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirdTest
+//
+//  Created by iOSDev on 15/4/20.
+//  Copyright (c) 2015年 iOSDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
